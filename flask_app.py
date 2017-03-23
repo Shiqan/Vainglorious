0b16fe10f58373263b38eb08dd0cb92ee9bc6763
ferron <ferron.saan@live.nl>
2017-03-23 15:59:17 +0100

    auto update

--- flask_app.py
+++ flask_app.py
@@ -1,19 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cache import Cache
 import config
 
 appname = "Vainglorious Meta"
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
-# cache = Cache(app,config={'CACHE_TYPE': 'simple'})
+cache = Cache(app,config={'CACHE_TYPE': 'simple'})
 
 app.config.from_object('config.DevelopmentConfig')
 # app.config.from_object('config.ProductionConfig')
 # app.config.from_envvar('YOURAPPLICATION_SETTINGS  ')
 app.secret_key = 'anotherplaintextpasswordftw'
 
-# import models, views
+import models, views
 
 #
 # @app.teardown_request
